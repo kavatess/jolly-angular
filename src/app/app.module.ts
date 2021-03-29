6548147212dce3f09132275014242f7bfba042c3
adp@loyco.cg <adp@loyco.cg>
2021-03-29 10:31:35 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BlocksModule } from './blocks/blocks.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BlocksModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
