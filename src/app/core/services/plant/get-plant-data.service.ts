84bf6e078f5ff04abebd4dc25f826d75e943b8c1
Dung Phan <lacxingau9a2pct@gmail.com>
2021-04-24 10:50:06 +0700

    recovered get services

---
+++ src/app/core/services/plant/get-plant-data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Plant } from '../../models/plant.model';
+import { HttpRequestService } from '../http-request.service';
+import { PLANT_REQUEST } from '../request-url-constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetPlantDataService {
+  constructor(private httpReq: HttpRequestService) { }
+  getPlantData(): Observable<Plant[]> {
+    return this.httpReq.createPostRequest(PLANT_REQUEST.getPlantData);
+  }
+}
