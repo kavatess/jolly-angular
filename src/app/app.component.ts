d9ac1afca42f9a53112da9a6f59a561f841508e8
adp@loyco.cg <adp@loyco.cg>
2021-04-26 16:20:07 +0700

    moved plant api to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SessionStorageService } from 'ngx-webstorage';
+import { PLANT_SESSION_COLLECTION } from './app-constants';
+import { GetPlantDataService } from './core/services/plant/get-plant-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jolly-angular';
+
+  constructor(private sessionStorage: SessionStorageService, private getPlantService: GetPlantDataService) { }
+
+  ngOnInit(): void {
+    if (!this.sessionStorage.retrieve(PLANT_SESSION_COLLECTION)) {
+      this.getPlantService.getPlantData().subscribe(plantArr => {
+        this.sessionStorage.store(PLANT_SESSION_COLLECTION, plantArr);
+      });
+    }
+  }
 }
